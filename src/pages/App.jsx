cc5a17515b1579401e33874a45462388515342e6
Glaucielle Sá <65506911+glauciellesa@users.noreply.github.com>
2022-12-12 16:45:55 +0100

    First Commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,5 @@
+import Index from "./Index";
+
+export default () => {
+  return <Index />;
+};
---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+export default () => {
+  return (
+    <nav>
+      <h1>Bob's Bristro</h1>
+      <ul>
+        <li>Menu</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
