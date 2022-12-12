adab750ef1a6234b0ff316efab00c094e54f9234
Glaucielle Sá <65506911+glauciellesa@users.noreply.github.com>
2022-12-12 18:09:35 +0100

    Project part 1-markup

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,36 @@
+import reactLogo from "../img/logo192.png";
+import styled from "styled-components";
+
 export default () => {
   return (
-    <nav>
-      <h1>Bob's Bristro</h1>
-      <ul>
-        <li>Menu</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <Logo>
+          <img src={reactLogo} alt="React's logo" />
+        </Logo>
+      </nav>
+      <Body>
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 100k stars on Github</li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </Body>
+    </>
   );
 };
+
+const Logo = styled.div`
+  margin: 20px;
+  img {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+const Body = styled.body`
+  margin: 20px;
+`;
